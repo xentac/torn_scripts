6032ec5e703c5a05229465aba4c4f27f4953f705
Jason Chu <jchu@xentac.net>
2025-10-25 16:41:44 +1300

    Remove style sheets and style attributes, not classes

--- remove_styles.js
+++ remove_styles.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         torn.com Remove all styling on torn.com
 // @namespace    xentac
-// @version      20251025.1
+// @version      20251025.2
 // @description  torn.com So we can "make the page go faster"
 // @author       xentac [3354782]
 // @match        https://www.torn.com/*
@@ -10,20 +10,17 @@
 
 "use strict";
 
-function removeStyling(node) {
-  //console.log(node);
-  if (node.querySelectorAll) {
-    node
-      .querySelectorAll("[style]")
-      .forEach((el) => el.removeAttribute("style"));
-    node
-      .querySelectorAll("[class]")
-      .forEach((el) => el.removeAttribute("class"));
-  }
+function removeStyling() {
+  document
+    .querySelectorAll('style, link[rel="stylesheet"]')
+    .forEach((e) => e.remove());
+  document
+    .querySelectorAll("[style]")
+    .forEach((e) => e.removeAttribute("style"));
 }
 
 function react(mutation) {
-  mutation.addedNodes.forEach(removeDarkStyling);
+  mutation.addedNodes.forEach(removeStyling);
 }
 
 function reactAll(mutations) {
@@ -32,7 +29,7 @@ function reactAll(mutations) {
 
 (function () {
   const target = document;
-  removeStyling(target);
+  removeStyling();
   const observer = new MutationObserver(reactAll);
   observer.observe(target, { childList: true, subtree: true });
   //observer.disconnect();
